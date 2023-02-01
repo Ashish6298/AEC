c186c9cc7058638e871b4e32520e17d935870d75
Ashish Goswami <116054951+Ashish6298@users.noreply.github.com>
2023-02-01 14:30:53 +0530

    Add files via upload

---
+++ Chocolate program.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int choc1,choc2,choc3,choc4,choc5,choc6;
+    cout<<"Enter the cost of chocolate 1 and chocolate 2:" <<endl;
+    cin>>choc1>>choc2;
+    if(choc1<choc2)
+    {
+    cout<<"chocolate 1 is cheaper:" <<endl;
+        
+    }
+
+    else
+    {
+           cout<<" chocolate 2 is cheaper:" <<endl;
+
+    }
+
+
+
+
+cout<<"Enter the cost of chocolate 3 and chocolate 4:" <<endl;
+    cin>>choc3>>choc4;
+    if(choc3<choc4)
+    {
+    cout<<"chocolate 3 is cheaper:" <<endl;
+        
+    }
+
+    else
+    {
+           cout<<" chocolate 4 is cheaper:" <<endl;
+
+    }
+
+
+
+
+
+cout<<"Enter the cost of chocolate 5 and chocolate 6 :" <<endl;
+    cin>>choc5>>choc6;
+    if(choc5<choc6)
+    {
+    cout<<"chocolate 5 is cheaper:" <<endl;
+        
+    }
+
+    else
+    {
+           cout<<" chocolate 6 is cheaper:" <<endl;
+
+    }
+
+
+
+return 0;             
+
+}
+
